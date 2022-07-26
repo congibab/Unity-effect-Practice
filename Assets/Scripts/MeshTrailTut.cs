ed16eeb5277a8444299cf73aaaf7f5b3261a9bb3
GoneHwi LEE <49512074+congibab@users.noreply.github.com>
2022-07-27 00:24:03 +0900

    CHARACTER TRAIL Effect作成

--- Assets/Scripts/MeshTrailTut.cs
+++ Assets/Scripts/MeshTrailTut.cs
@@ -1,18 +1,82 @@
+using System;
+using Cysharp.Threading.Tasks;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class MeshTrailTut : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float activeTime = 2f;
+
+    [Header("Mesh Related")]
+    public float meshRefreshRate = 0.1f;
+    public float meshDestroyDelay = 3.0f;
+    public Transform positionToSpawn;
+
+    [Header("Shader Related")]
+    public Material mat;
+    public string shaderVerRef;
+    public float shaderVarRate = 0.1f;
+    public float shaderVarRefreshRate = 0.05f;
+
+    private bool isTrailActive;
+    private SkinnedMeshRenderer[] meshRenderers;
+
+    private void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space) && !isTrailActive)
+        {
+            isTrailActive = true;
+            ActivateTrail(activeTime);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    async UniTask ActivateTrail(float timeActive)
     {
-        
+        Debug.Log("start");
+
+        while (timeActive > 0)
+        {
+            timeActive -= meshRefreshRate;
+
+            if (meshRenderers == null)
+            {
+                meshRenderers = GetComponentsInChildren<SkinnedMeshRenderer>();
+            }
+
+            for (int i = 0; i < meshRenderers.Length; i++)
+            {
+                var gObj = new GameObject();
+                gObj.transform.SetPositionAndRotation(positionToSpawn.position, positionToSpawn.rotation);
+
+                var mr = gObj.AddComponent<MeshRenderer>();
+                var mf = gObj.AddComponent<MeshFilter>();
+
+                Mesh mesh = new Mesh();
+                meshRenderers[i].BakeMesh(mesh);
+                mf.mesh = mesh;
+                mr.material = mat;
+
+                AnimateMaterialFloat(mr.material, 0, shaderVarRate, shaderVarRefreshRate);
+
+                Destroy(gObj, meshDestroyDelay);
+            }
+
+            await UniTask.Delay(TimeSpan.FromSeconds(meshRefreshRate), ignoreTimeScale: false);
+        }
+
+        isTrailActive = false;
+    }
+
+    async UniTask AnimateMaterialFloat(Material mat, float goal, float rate, float refrehRate)
+    {
+        float valueToAnimate = mat.GetFloat(shaderVerRef);
+
+        while (valueToAnimate > goal)
+        {
+            valueToAnimate -= rate;
+            mat.SetFloat(shaderVerRef, valueToAnimate);
+            await UniTask.Delay(TimeSpan.FromSeconds(refrehRate), ignoreTimeScale: false);
+        }
     }
 }
